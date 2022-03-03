ca34f77c9f18c26564466a52bb43874aeb5e2ea6
Benjamin Bolbrinker <benjamin.bolbrinker@hiddb.com>
2022-03-03 08:34:36 +0100

    add indices and documents

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from curses import doupdate
 import requests
 import jwt
 from threading import Timer
@@ -14,13 +15,11 @@ baseUrl = 'https://api.hiddb.io'
 postHeaders = { 'Content-Type' : 'application/json' }
 
 
-
 class HIDDB(object):
     @classmethod
     async def create(cls, key, secret):
         self = HIDDB()
         self.state = State(self, key, secret)
-        self.appClient = requests.Session()
 
         await self._machine_login(key, secret)
         return self
@@ -59,7 +58,7 @@ class HIDDB(object):
                 return await resp.json()
 
 
-    async def get_databases(self):
+    async def list_databases(self):
         url = baseUrl
         path = f"/database"
         method = "get"
@@ -145,7 +144,169 @@ class HIDDB(object):
             session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
             async with req(path) as resp:
                 return await resp.json()
+    
 
+    async def create_collection(self, database_id: str, name: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection"
+        method = "post"
+
+        body = {
+            "collection_id": name,
+        }
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path, json=body, headers=postHeaders) as resp:
+                return await resp.json()
+
+
+    async def list_collections(self, database_id: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection"
+        method = "get"
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path) as resp:
+                return await resp.json()
+
+
+    async def get_collection(self, database_id: str, name: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{name}"
+        method = "get"
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path) as resp:
+                return await resp.json()
+    
+
+    async def delete_collection(self, database_id: str, name: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{name}"
+        method = "delete"
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path) as resp:
+                return await resp.json()
+
+
+    async def create_index(self, database_id: str, collection: str, field: str, dimension: int):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{collection}/index"
+        method = "post"
+
+        body = {
+            "field_id": field,
+            "dimension": dimension,
+        }
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path, json=body, headers=postHeaders) as resp:
+                return await resp.json()
+
+
+    async def list_indices(self, database_id: str, collection: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{collection}/index"
+        method = "get"
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path) as resp:
+                return await resp.json()
+
+
+    async def get_index(self, database_id: str, collection: str, name: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{collection}/index/{name}"
+        method = "get"
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path) as resp:
+                return await resp.json()
+    
+
+    async def delete_index(self, database_id: str, collection: str, name: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{collection}/index/{name}"
+        method = "delete"
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path) as resp:
+                return await resp.json()
+
+
+    async def insert_document(self, database_id: str, collection: str, document: dict):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{collection}/document"
+        method = "post"
+
+        body = {
+            "documents": [document],
+        }
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path, json=body, headers=postHeaders) as resp:
+                return await resp.json()
+
+
+    async def search_nearest_documents(self, database_id: str, collection: str, field: str, vector, max_neighbors=10):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{collection}/document/search"
+        method = "post"
+
+        body = {
+            "vector": vector,
+            "field": field,
+            "max_neighbors": max_neighbors
+        }
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path, json=body, headers=postHeaders) as resp:
+                return await resp.json()
+
+
+    async def get_document(self, database_id: str, collection: str, id: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{collection}/document/{id}"
+        method = "get"
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path) as resp:
+                return await resp.json()
+    
+
+    async def get_document(self, database_id: str, collection: str, id: str):
+        url = f"https://{database_id}.hiddb.io"
+        path = f"/collection/{collection}/document/{id}"
+        method = "delete"
+
+        async with aiohttp.ClientSession(url) as session:
+            req = getattr(session, method)
+            session.headers.update({'Authorization' : f'Bearer {self.state.access_token}'})
+            async with req(path) as resp:
+                return await resp.json()
 
 class State:
     def __init__(self, hiddb: HIDDB, key: str, secret: str):
@@ -180,7 +341,7 @@ async def main():
     secret="dxk59OkHtq-LZ8CuG2qH"
     hiddb = await HIDDB.create(key, secret)
     # print(await hiddb.create_database("tesdb"))
-    # print(await hiddb.get_databases())
+    # print(await hiddb.list_databases())
     # print(await hiddb.get_database("u92cccov5tsg14tq2l"))
     # print(await hiddb.delete_database("u92cccov5tsg14tq2l"))
     # print(await hiddb.get_instances())
